d85e3c5f398446fda08ac6cad8cde63844941823
wonderbloc <119789456+wonderbloc@users.noreply.github.com>
2026-03-25 19:34:02 +0100

    BWAAAAH

--- JHTM DEMO/main.py
+++ JHTM DEMO/main.py
@@ -4,21 +4,34 @@ from pygame.locals import *
 pygame.init()
 
 ################################################################################
-# Chargement des sprite
-
+## Chargement des sprite
 spr_background = pygame.image.load("sprites/background.jpg") 
 spr_perso = pygame.image.load("sprites/tete.png")
 spr_icone = pygame.image.load("sprites/crane.png")
+##Création des Classe
+class obj_player(pygame.sprite.Sprite): #Création du perso
+    def __init__(self): #Les variable du perso
+        super().__init__ #Permet de relier un sprite plus tard
+        self.health = 3 #PV du perso
+        self.mov=[0,0]  #Vélocité du perso [x,y]
+        self.pos=[0.0]
+        self.size=100 #Taille du perso
+        self.image= spr_perso #relie le sprite
+        self.rect =self.image.get_rect() #Choppe le rectangle de l'image (pour les collision)
+
+class obj_game():
+    def __init__(self):
+        self.player = obj_player()
+
 ####Variables
-#Systeme
+#Fenetre
 screen_size = (1920-50,1080-50) #Taille de la fenetre (-50 parce qu'on veut quand meme appuyer sur quitter)
+screen = pygame.display.set_mode(screen_size)
 fps = 24
-perso_coo=[500,500] #coordonnée du perso
-perso_mov=[0,0] #Vélocité du perso
-perso_size=100 #Taille du perso
 #Tiles
-margin=5
-tile_size= perso_size+margin
+gap=30
+margin=30
+tile_size= 100+margin
 tile_number=3 #nombre de tile dans une ligne du carée
 ####Définition des touche
 key_down=[115,1073741905] # S + Bas 
@@ -30,12 +43,13 @@ keys=[key_right,key_left,key_up,key_left] #liste des clé
 #Fonctions
 def pressed(key):
     return event.key in key
+
+
 # Ouverture de la fenêtre Pygame et collage du fond
 
-screen = pygame.display.set_mode(screen_size)
+#Création des instance
 
-fond=spr_background.convert()
-
+game= obj_game()
 
 # Rafraîchissement de l'écran
 pygame.display.set_caption("JHTM") #Nom du projet
@@ -65,15 +79,15 @@ while run:
                     ##pygame.mixer.music.unpause()
                     music = True
              #Mouvement
-            perso_mov[0],perso_mov[1]=(pressed(key_right)-pressed(key_left))*tile_size,(pressed(key_down)-pressed(key_up))*tile_size #mouvement x et mouvement Y
-            perso_coo[0],perso_coo[1]=perso_coo[0]+perso_mov[0],perso_coo[1]+perso_mov[1] #applique les mouvement
+            game.player.mov[0],game.player.mov[1]=(pressed(key_right)-pressed(key_left))*(tile_size+gap),(pressed(key_down)-pressed(key_up))*(tile_size+gap) #mouvement x et mouvement Y
+            game.player.rect.x,game.player.rect.y=game.player.rect.x+game.player.mov[0],game.player.rect.y+game.player.mov[1] #applique les mouvement
             print(pressed(key_right), event.key)
   
     # Instructions
     # ...............
     
-    screen.fill((255,255,255))
-    screen.blit(pygame.transform.scale_by(spr_perso,(tile_size/spr_perso.size[0],tile_size/spr_perso.size[1])),perso_coo)
+    screen.fill((0,0,255))
+    screen.blit(pygame.transform.scale_by(spr_perso,(game.player.size/spr_perso.size[0],game.player.size/spr_perso.size[1])),game.player.rect)
 
     # Re-collage des élémentsù
 
